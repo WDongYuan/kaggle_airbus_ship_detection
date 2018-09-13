68f01e9f081648558c644425551b824450d3db6e
WeidongYuan <weidongy@andrew.cmu.edu>
2018-09-12 23:07:00 -0700

    sync

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def TrainModel(model, optimizer, train_dataloader, valid_dataloader, decay_step,
 
 	model.cuda()
 
-	loss_func = nn.NLLLoss(reduction="none")
+	loss_func = nn.NLLLoss()
 	for epoch in range(total_epoch):
 		print("##################################")
 		print("epoch "+str(epoch))
@@ -38,10 +38,12 @@ def TrainModel(model, optimizer, train_dataloader, valid_dataloader, decay_step,
 			log_prob = model(sample_batch["img"].cuda())
 			# print(log_prob.size())
 			# print(sample_batch["weight_img"].size())
-			weight_log_prob = torch.mul(log_prob,sample_batch["weight_img"].unsqueeze(1).float().cuda())
+			log_prob = torch.mul(log_prob,sample_batch["weight_img"].unsqueeze(1).float().cuda())
 			loss = loss_func(log_prob,sample_batch["label_img"].long().cuda())
-			loss_mean = loss.mean()
-			print("%f,%d"%(loss_mean.data.cpu().numpy(),(batch_count+1)*batch_size), end=" ", flush=True)
+			# loss_mean = loss.mean()
+			print("%f,%f,%d"%
+				(loss.mean().data.cpu().numpy(),classify_accuracy(log_prob.data.numpy(),sample_batch["label_img"]), (batch_count+1)*batch_size),
+				end=" ", flush=True)
 			loss.backward()
 			optimizer.step()
 
