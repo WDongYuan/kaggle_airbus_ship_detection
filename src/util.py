dd3a4d4453d7bd2650aa81543e626c980b48c53d
WeidongYuan <weidongy@andrew.cmu.edu>
2018-09-13 23:19:12 -0700

    sync

--- src/util.py
+++ src/util.py
@@ -2,5 +2,5 @@ train_img_dir = "../data/train/"
 train_label_file = "../data/train_ship_segmentations.csv"
 test_img_dir = "../data/test"
 valid_ratio = 0.2
-batch_size = 10
+batch_size = 20
 learning_rate = 0.01
