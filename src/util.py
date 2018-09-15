fb8d27ac31891e4a6cde5b2f78d644c10725d184
Ubuntu <ubuntu@ip-172-31-31-251.ec2.internal>
2018-09-15 23:33:20 +0000

    sync

--- src/util.py
+++ src/util.py
@@ -5,10 +5,10 @@ valid_ratio = 0.2
 batch_size = 20
 learning_rate = 0.0001
 decay_rate = 0.999
-saved_model = "./saved_model/model_0_51866"
+saved_model = "./saved_model/model_400_54119"
 img_split_parts = 9
 weight_ratio = 15 # must larger than 8
 
 
-model_flag = "train"
+model_flag = "predict"
 continue_train_learning_rate = 0.00005
