1f2357a450c509c36217f646eb06b97b3df8770b
WeidongYuan <weidongy@andrew.cmu.edu>
2018-09-17 23:27:07 -0700

    sync

--- src/data_processing.py
+++ src/data_processing.py
@@ -153,6 +153,8 @@ class TestImageData:
             # tmp_img = torch.from_numpy(tmp_img).permute(0,3,1,2).float()
 
             tmp_img = self.get_img_part(np.array(tmp_img),768,768,img_split_parts,5)
+            print(tmp_img)
+            print(transforms.functional.to_tensor(tmp_img))
             tmp_img = Image.fromarray(tmp_img)
             tmp_img = transforms.functional.to_tensor(tmp_img)
             print(tmp_img)
