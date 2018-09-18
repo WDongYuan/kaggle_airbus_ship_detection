020a6950acd202cead97f2ccefd2f75bf81ccc95
WeidongYuan <weidongy@andrew.cmu.edu>
2018-09-17 22:14:27 -0700

    sync

--- src/main.py
+++ src/main.py
@@ -200,7 +200,7 @@ if __name__=="__main__":
 
 	elif model_flag == "test":
 		model = torch.load(saved_model)
-		ModelTest(model,test_dataloader)
+		ModelTest(model,train_dataloader)
 
 	# counter = 0
 	# for i_batch, sample_batch in enumerate(train_dataloader):
