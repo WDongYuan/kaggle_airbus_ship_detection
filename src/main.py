b5af065b79ae6ff3dffb04a4b61881929fec1fd8
WeidongYuan <weidongy@andrew.cmu.edu>
2018-09-17 21:23:41 -0700

    sync

--- src/main.py
+++ src/main.py
@@ -49,7 +49,7 @@ def ModelTest(model,test_dataloader):
 				b,sub,c,h,w = sample_batch["img"].size()
 				print(sample_batch["img"].size())
 				sample_batch["img"] = sample_batch["img"].view(b*sub,c,h,w)
-			print(sample_batch["img"][0])
+			print(sample_batch["img"].size())
 			return
 			log_prob = model(sample_batch["img"].cuda())
 			log_prob = log_prob.data.cpu().numpy()
