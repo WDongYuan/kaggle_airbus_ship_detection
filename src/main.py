9e3f6a65d452033abfca0b4d24a5b9d3c933473d
WeidongYuan <weidongy@andrew.cmu.edu>
2018-09-17 20:38:05 -0700

    sync

--- src/main.py
+++ src/main.py
@@ -187,6 +187,7 @@ if __name__=="__main__":
 	elif model_flag == "test":
 		test_dataset = TestImageData(test_img_dir)
 		test_dataloader = DataLoader(test_dataset, batch_size = batch_size, shuffle=False, num_workers=0)
+		
 		model = torch.load(saved_model)
 		ModelTest(model,test_dataloader)
 
